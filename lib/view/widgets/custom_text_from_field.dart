7bce9e607de64124383f91825d9f04e8c778a611
Ali Qassem <aliyeadqassem@gmail.com>
2024-06-24 11:51:35 +0300

    Afer after the FKN reset shit & before gradle 2

--- lib/view/widgets/custom_text_from_field.dart
+++ lib/view/widgets/custom_text_from_field.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 import 'package:warehouse_manegment_system/constans.dart';
 
 //
-//
+
 class CustomTextFromField extends StatelessWidget {
   CustomTextFromField({
     Key? key,
